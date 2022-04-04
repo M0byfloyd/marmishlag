91dfbdf6512a77514fb62f626b5b47467bee32a5
Oceloott <60733546+Oceloott@users.noreply.github.com>
2022-04-04 16:03:29 +0200

    save header

--- wordpress/wp-content/themes/marmishlag-theme/header.php
+++ wordpress/wp-content/themes/marmishlag-theme/header.php
@@ -1,34 +1,40 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php wp_head(); ?>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-            <?php wp_nav_menu([
-                'theme_location' => 'custom_header',
-                'menu_class' => 'navbar-nav me-auto mb-2 mb-lg-0',
-                'container' => false
-            ]); ?>
-
-            <?php get_search_form(); ?>
-
+    <nav class="navbar navbar-expand-lg navbar-box">
+        <div class="container-fluid justify-content-between">
+            <a class="navbar-brand navbar-title" href="#">MIJOTON</a>
+            <button class="navbar-toggler navbar-burger" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon navbar-burger-icons"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <?php get_search_form(); ?>
+                <a href="#" class="navbar-connexion" id="open-search">
+                    <svg viewBox="0 0 32 32" class="navbar-connexion-user">
+                        <title />
+                        <g id="about">
+                            <path d="M16,16A7,7,0,1,0,9,9,7,7,0,0,0,16,16ZM16,4a5,5,0,1,1-5,5A5,5,0,0,1,16,4Z" />
+                            <path
+                                d="M17,18H15A11,11,0,0,0,4,29a1,1,0,0,0,1,1H27a1,1,0,0,0,1-1A11,11,0,0,0,17,18ZM6.06,28A9,9,0,0,1,15,20h2a9,9,0,0,1,8.94,8Z" />
+                        </g>
+                    </svg>
+                    Connexion
+                </a>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
 
-<div class="container p-5">
+    <div class="container p-5">
--- wordpress/wp-content/themes/marmishlag-theme/searchform.php
+++ wordpress/wp-content/themes/marmishlag-theme/searchform.php
@@ -1,5 +1,7 @@
-<form action="<?php esc_url(home_url('/')) ?>" id="searchForm">
-    <input type="search" placeholder="Chercher" name="s" value="<?= get_search_query() ?>">
+<div class="search-box">
+    <form action="<?php esc_url(home_url('/')) ?>" id="searchForm">
+        <input class="search-input" id="open-search" value="<?= get_search_query() ?>" type="search" name="s" placeholder="Search">
+        <label class="search-button search-label"   for="open-search"><span class="search-logo">&#9906;</span><span class="search-label-text">Recherche</span></label>
+    </form>
+</div>
 
-    <button type="submit">Chercher</button>
-</form>
