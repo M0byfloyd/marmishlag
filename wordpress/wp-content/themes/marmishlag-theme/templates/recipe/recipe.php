26a058acb3683bb12b2739e509879928dadeb024
Aubin <aolivrie77@gmail.com>
2022-04-04 15:24:45 +0200

    Footer

---
+++ wordpress/wp-content/themes/marmishlag-theme/templates/recipe/recipe.php
@@ -0,0 +1,8 @@
+<div class="recette card">
+  <img src="https://images.unsplash.com/photo-1616169776580-c86189ee67b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
