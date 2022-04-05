3858efd51ccbff73dc39f15125c74093118dc653
Oceloott <60733546+Oceloott@users.noreply.github.com>
2022-04-05 07:33:54 +0200

    header done

--- wordpress/wp-content/themes/marmishlag-theme/searchform.php
+++ wordpress/wp-content/themes/marmishlag-theme/searchform.php
@@ -1,7 +1,7 @@
 <div class="search-box">
     <form action="<?php esc_url(home_url('/')) ?>" id="searchForm">
-        <input class="search-input" id="open-search" value="<?= get_search_query() ?>" type="search" name="s" placeholder="Search">
-        <label class="search-button search-label"   for="open-search"><span class="search-logo">&#9906;</span><span class="search-label-text">Recherche</span></label>
+        <input class="search-input" id="open-search" value="<?= get_search_query() ?>" type="search" name="s" placeholder="Rechercher">
+        <label class="search-button search-label"   for="open-search"><span class="search-logo">&#9906;</span></label>
     </form>
 </div>
 
