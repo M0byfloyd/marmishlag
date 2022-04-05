bd3bcdb3835da940a6c5bcace34fe211a67fc4f0
m0byfloyd <alexandre.robert2810@gmail.com>
2022-04-05 16:36:32 +0200

    srlg,sn

--- wordpress/wp-content/themes/marmishlag-theme/templates/recipe/thumbnail.php
+++ wordpress/wp-content/themes/marmishlag-theme/templates/recipe/thumbnail.php
@@ -1,23 +1,7 @@
 <div class="thumbnail card">
- <div class="card-body">
-    <p class="card-text">Auteur de la thumbnail</p>
-    <h5 class="card-title">Titre de la thumbnail</h5>
-    <div class="thumbnail-note">
-        <svg class="filled-star" width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11 0.375L7.87188 6.71375L0.880005 7.72437L5.94001 12.6606L4.74375 19.625L11 16.3387L17.2563 19.625L16.06 12.6606L21.12 7.73125L14.1281 6.71375L11 0.375Z"/>
-        </svg>
-        <svg class="filled-star" width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11 0.375L7.87188 6.71375L0.880005 7.72437L5.94001 12.6606L4.74375 19.625L11 16.3387L17.2563 19.625L16.06 12.6606L21.12 7.73125L14.1281 6.71375L11 0.375Z"/>
-        </svg>
-        <svg class="filled-star" width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11 0.375L7.87188 6.71375L0.880005 7.72437L5.94001 12.6606L4.74375 19.625L11 16.3387L17.2563 19.625L16.06 12.6606L21.12 7.73125L14.1281 6.71375L11 0.375Z"/>
-        </svg>
-        <svg class="empty-star" width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11 0.375L7.87188 6.71375L0.880005 7.72437L5.94001 12.6606L4.74375 19.625L11 16.3387L17.2563 19.625L16.06 12.6606L21.12 7.73125L14.1281 6.71375L11 0.375Z"/>
-        </svg>
-        <svg class="empty-star" width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11 0.375L7.87188 6.71375L0.880005 7.72437L5.94001 12.6606L4.74375 19.625L11 16.3387L17.2563 19.625L16.06 12.6606L21.12 7.73125L14.1281 6.71375L11 0.375Z"/>
-        </svg>
-    </div>
-  </div>
+    <a href="<?= get_the_permalink() ?>" class="card-body">
+        <p class="card-text"><?= the_author() ?></p>
+        <h5 class="card-title"><?= the_title() ?></h5>
+        <?php do_action('marmiplug-difficulty'); ?>
+    </a>
 </div>
